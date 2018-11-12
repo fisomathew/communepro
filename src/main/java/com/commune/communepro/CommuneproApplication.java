a2bb74835b715d69809fdec40684ff3421daf048
Fiso Mathew <fiso.mathew@gmail.com>
2018-11-12 16:31:11 +0530

    Initial commit

---
+++ src/main/java/com/commune/communepro/CommuneproApplication.java
@@ -0,0 +1,12 @@
+package com.commune.communepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuneproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuneproApplication.class, args);
+	}
+}
