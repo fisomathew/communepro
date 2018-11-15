062770b586de664a11355e6416c242f9b93ecd94
Fiso Mathew <fiso.mathew@gmail.com>
2018-11-15 13:43:28 +0530

    Spring security config commit

---
+++ src/main/java/com/commune/communepro/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.commune.communepro.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /**
+         * This settings are for getting h2 console. Can be removed.
+         */
+        http.headers().frameOptions().disable();
+        /**
+         * Spring security related configuration.
+         */
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**" ,"/fonts/**", "/img/**", "/js/**", "/index").permitAll()
+                .antMatchers("/pboard/**").hasRole("Board")
+                .and()
+                .formLogin()
+                .loginPage("/login").failureUrl("/login-error").and()
+                .csrf().disable();
+    }
+
+}
